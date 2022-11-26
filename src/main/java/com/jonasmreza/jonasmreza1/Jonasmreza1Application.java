8fe10a0f5754241bcd05b858e82e2c30686e9bd8
jonasmreza <jonasmreza@gmail.com>
2022-11-26 12:59:40 -0600

    ad logic and test

---
+++ src/main/java/com/jonasmreza/jonasmreza1/Jonasmreza1Application.java
@@ -0,0 +1,13 @@
+package com.jonasmreza.jonasmreza1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jonasmreza1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jonasmreza1Application.class, args);
+	}
+
+}
